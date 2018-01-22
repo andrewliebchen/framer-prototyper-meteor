ade369d16d92e0e5cb280929f11924d12003fd35
Andrew Liebchen <andrewliebchen@gmail.com>
2018-01-21 16:40:47 -0800

    WIP

--- imports/ui/components/Button.jsx
+++ imports/ui/components/Button.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
+import styled from "styled-components";
 
 import "../styles/Button.css";
 
+const Element = styled.button`
+  appearance: none;
+  background-color: transparent;
+  border: 1px solid;
+  border-color: ${props =>
+    props.negative ? "var(--color-red)" : "var(--color-accent)"};
+  color: ${props =>
+    props.negative ? "var(--color-red)" : "var(--color-accent)"};
+  display: inline-block;
+  line-height: 3em;
+  font-weight: 500;
+  text-align: center;
+  border-radius: 3em;
+  padding: 0 1em;
+  font-size: 1em;
+  cursor: pointer;
+  text-transform: uppercase;
+  letter-spacing: var(--letter-spacing);
+  user-select: none;
+
+  &:hover {
+    background-color: var(--color-accent);
+    color: white;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: var(--outline-focus);
+  }
+`;
+
 const Button = props => (
-  <button
+  <Element
     className={classnames({
       Button: true,
       Block: props.block,
@@ -16,7 +48,7 @@ const Button = props => (
     disabled={props.disabled}
   >
     {props.label}
-  </button>
+  </Element>
 );
 
 Button.propTypes = {
